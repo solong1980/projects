84f862d1de3f6c9c4c7130a06d7d88a950e30344
solong <solong1980@163.com>
2017-05-28 11:14:53 +0800

    1.增加webupload文件上传功能demo

---
+++ agent/agent-management/src/main/java/com/yz/boster/controller/upload/FileUploadController.java
@@ -0,0 +1,72 @@
+package com.yz.boster.controller.upload;
+
+import java.io.File;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.yz.boster.commons.base.BaseController;
+import com.yz.boster.model.vo.UploadConfigVo;
+
+@Controller
+@RequestMapping("/file")
+public class FileUploadController extends BaseController {
+	@GetMapping("/uploadConfig")
+	@ResponseBody
+	public UploadConfigVo uploadConfig(Model model) {
+		UploadConfigVo configVo = new UploadConfigVo();
+		configVo.setMaxSize(10000);
+		configVo.setFileMaxCount(10);
+		return configVo;
+	}
+
+	@GetMapping("/webUploadFile")
+	public String webUploadFile(Model model) {
+		return "file/webupload";
+	}
+	
+	@GetMapping("/plUploadFile")
+	public String plUploadFile(Model model) {
+		return "file/plupload";
+	}
+	
+	@PostMapping(value = "/upload")
+	@ResponseBody
+	public String uploadFileHandler(@RequestParam("file") MultipartFile[] files,
+			HttpSession session) {
+		if (files != null && files.length > 0) {
+			String path = session.getServletContext().getRealPath("/Images");
+			for (MultipartFile file : files) {
+				try {
+					if (file.isEmpty())
+						continue;
+					// 文件存放服务端的位置
+					File dir = new File(path + File.separator + "tmpFiles");
+					if (!dir.exists())
+						dir.mkdirs();
+					// 写文件到服务器
+					File serverFile = new File(dir.getAbsolutePath()
+							+ File.separator + file.getOriginalFilename());
+					file.transferTo(serverFile);
+				} catch (Exception e) {
+					e.printStackTrace();
+					break;
+				}
+			}
+			
+			return UUID.randomUUID().toString();
+		} else {
+			return "";
+		}
+	}
+
+}
